7bd722c1b0ad88bfb511171a9da997fa5bae2d2d
Arka Bhattacharya <rehanarka07@gmail.com>
2026-06-30 14:19:36 +0530

    Add files via upload

---
+++ Comprehension.py
@@ -0,0 +1,28 @@
+import random
+
+# numbers = [1, 2, 3]
+# new_item = [n + 1 for n in numbers]
+# print(new_item)
+
+# name = "Arka"
+# letter_item = [n for n in name]
+# print(new_item)
+
+# list1 = [i * 2 for i in range(1, 5)]
+# print(list1)
+
+# num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# list2 = [i for i in num if i % 2 == 0]
+# print(list2)
+
+# name = ["Arka", "Rudra", "Lakshay", "Swaroop", "Satish", "Shantanu"]
+# list3 = [n for n in name if len(n) < 7]
+# list4 =[n.upper() for n in name if len(n) > 4 ]
+# print(list4)
+
+# student_scores = {student : random.randint(1, 100) for student in name}
+# print(student_scores)
+# passed_students = {student : score for (student, score) in student_scores.items() if score >= 45}
+# print("\n")
+# print(passed_students)
+
